5413d6f817dc63e9f22ac8de1bee20d7d632fc5e
julio cesar arrieta santiago <jarrieta@finanzas.cdmx.gob.mx>
2022-06-24 17:35:54 -0500

    search component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import  Listar  from "./componentes/Listar";
 import Crear from "./componentes/Crear";
+import SearchBar from "./components/SearchBar";
 import {  BrowserRouter as Router,  Route,  Routes} from "react-router-dom";
 
 function App() {
@@ -12,11 +13,13 @@ function App() {
                   <div className="nav navbar-nav">
                       <a className="nav-item nav-link active" href="/">Sistema <span className="sr-only">(current)</span></a>
                       <a className="nav-item nav-link" href="Crear">Crear Empleado</a>
+                      <a className="nav-item nav-link" href="Buscar">Search</a>
                   </div>
               </nav>
                 <Routes>
                 <Route path='/' element={<Listar></Listar>}> </Route>
                 <Route path='/Crear' element={<Crear></Crear>}> </Route>
+                <Route path='/Buscar' element={<SearchBar></SearchBar>}> </Route>
                 </Routes>
             </div>
         </Router>
